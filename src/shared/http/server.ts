3683045446e1c4823f50b8f56e7602cb2ee6f814
EderSouza93 <ederaldo.souza93@gmail.com>
2025-06-28 13:28:11 -0300

    feat: create error instance and server

---
+++ src/shared/http/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+import 'express-async-errors';
+import ErrorHandleMiddleware from '@shared/middlewares/ErrorHandleMiddleware';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+app.use(ErrorHandleMiddleware.handleError);
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333!')
+});
