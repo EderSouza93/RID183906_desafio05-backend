85491e084e44a5392b356ba6ecd934bd87e1d7e2
EderSouza93 <ederaldo.souza93@gmail.com>
2025-06-28 14:16:45 -0300

    feat: adding docker-compose

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use(routes);
 app.use(ErrorHandleMiddleware.handleError);
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333!')
+app.listen(3000, () => {
+  console.log('Server started on port 3000!')
 });
