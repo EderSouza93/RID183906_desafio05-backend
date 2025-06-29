35a4d4a61d40a6618f6c12209f4df3630419ab46
EderSouza93 <ederaldo.souza93@gmail.com>
2025-06-29 17:22:08 -0300

    feat: controllers and routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
-import { Router } from "express";
+import booksRoutes from "@books/infra/http/routes/BookRoutes";
+import { Router, Request, Response } from "express";
 
 const routes = Router();
 
-routes.get('/health', (request, response) => {
+routes.get('/health', (request: Request, response: Response) => {
   response.json({ message: "Hello Dev! I'm Alive!" });
 });
+routes.use('/livros', booksRoutes)
 
 export default routes
